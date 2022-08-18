0ce0f24ed97c931991458aa99e4b11a05e912c90
onushukvyatcheslav <onushukvyatcheslav@gmail.com>
2022-08-18 22:35:54 +0300

    код для завдання 7

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿//Создайте приложение, проверяющее текст на недопустимые слова. Если недопустимое слово найдено, оно
+//должно быть заменено на набор символов *. По итогам работы приложения необходимо показать статистику действий.
+using System;
 using System.Text;
+using static System.Console;//щоб не писати консоле кожен раз
 
 
 namespace Arrays
@@ -8,7 +11,44 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            OutputEncoding = Encoding.Unicode;//для кирилиці
 
+            WriteLine("Введіть текст англійською ");
+
+            string numb = Convert.ToString(ReadLine());
+            string[] substrings = numb.Split(" ");
+
+            WriteLine("Введіть недопустиме слово з великої а потім з малої літери \nПриклад : Die die");
+            string word = Convert.ToString(ReadLine());
+            string[] subword = word.Split(" ");
+            int count = 0;
+            string[] forbword = new string[word.Length];
+
+            for (int i = 0; i < subword[0].Length; i++)
+            {
+                forbword[i] = "*";
+            }
+            string a = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                a += forbword[i];
+
+            }
+           
+
+            for (int i = 0; i < substrings.Length; i++)
+            {
+                if (substrings[i] == subword[0] || substrings[i] == subword[1])
+                {
+                    substrings[i] = a;
+                    count++;
+                }
+            }
+            for (int i = 0; i < substrings.Length; i++)
+            {
+                Write($"{substrings[i]} ");
+            }
+            Write($"Статистика : {count} заміни  забороненого слова ");
 
         }
     }
