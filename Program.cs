f43ffa034cb9ecbff55c1780b42f580d5de76f80
onushukvyatcheslav <onushukvyatcheslav@gmail.com>
2022-08-19 16:19:20 +0300

    добавив код для задачі 6

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿//Пользователь с клавиатуры вводит некоторый текст.Приложение должно изменять регистр первой буквы каждого предложения на букву в верхнем регистре.
+using System;
 using System.Text;
+using static System.Console;//щоб не писати консоле кожен раз
 
 
 namespace Arrays
@@ -8,8 +10,36 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            OutputEncoding = Encoding.Unicode;//для кирилиці
 
+            WriteLine("Введіть текст");
+            string text = Convert.ToString(ReadLine());
 
+           StringBuilder edText = new StringBuilder(text);
+            
+            edText[0] = Char.ToUpper(edText[0]);//відразу змінію регістр першої букви тексту  бо в цикл немає сенсу  заносити цю операцію 
+            int size = edText.Length;          
+            size--;
+            for (int i = 1; i < edText.Length; i++)
+            {
+                if (i == size)
+                {
+                    break;
+                }
+               else  if (edText[i] == '.' && edText[i+1]==' ' ) 
+                {
+                edText[i+2]= Char.ToUpper(edText[i+2]);
+                    
+                }
+               else if (edText[i] == '.' && edText[i + 1] != ' ' )
+                {
+                    edText[i + 1] = Char.ToUpper(edText[i + 1]);
+
+                }
+
+            }
+            edText.ToString();
+            Write($"{edText}");
         }
     }
 }
